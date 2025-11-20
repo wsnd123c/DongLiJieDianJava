f24343b3739d89adeff8a3994ad413b869795719
邓可言 <319505551@qq.com>
2025-11-20 17:30:19 +0800

    上传

--- spring6-004-scope/src/test/java/com/powernode/spring6/test/test.java
+++ spring6-004-scope/src/test/java/com/powernode/spring6/test/test.java
@@ -1,4 +1,14 @@
 package com.powernode.spring6.test;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class test {
+   @Test
+    public void test1(){
+       ApplicationContext  context = new ClassPathXmlApplicationContext("beans.xml");
+       n
+   }
+
 }
